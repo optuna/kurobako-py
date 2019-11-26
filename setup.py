d15abb1e31839ee177f6fef4db9a615fc898513c
Takeru Ohta <phjgt308@gmail.com>
2019-11-26 23:26:39 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,24 @@
+import os
 from setuptools import find_packages
 from setuptools import setup
+
+
+def get_long_description() -> str:
+    readme_filepath = os.path.join(os.path.dirname(__file__), 'README.md')
+    with open(readme_filepath) as f:
+        return f.read()
+
 
 setup(
     name="kurobako",
     version="0.0.1",
+    description="A Python library to help implementing kurobako's solvers and problems",
+    long_description=get_long_description(),
+    long_description_content_type='text/markdown',
+    author='Takeru Ohta',
+    author_email='phjgt308@gmail.com',
+    url='https://github.com/sile/kurobako-py',
+    license='MIT',
     packages=find_packages(),
+    install_requires=['numpy'],
 )
